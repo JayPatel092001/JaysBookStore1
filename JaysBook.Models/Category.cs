d415d01365c4dd0f5efd5e304ef3903c7b569bb6
Jay Patel <W0778384@myscc.ca>
2021-12-05 10:57:02 -0500

    Craete Category and ICategory

--- JaysBook.Models/Category.cs
+++ JaysBook.Models/Category.cs
@@ -10,10 +10,14 @@ namespace JaysBook.Models
 {
     public class Category
     {
+        
+
         [Key]
+        public int Id { get; set; }
         [Display(Name = "Category Name")]
         [Required]
         [MaxLength(50)]
         public string Name { get; set; }
+        
     }
 }
