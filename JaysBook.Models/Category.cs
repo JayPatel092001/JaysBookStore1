f7845d6df937f846a2ea79969b38e144135798d5
Jay Patel <W0778384@myscc.ca>
2021-12-05 10:13:23 -0500

    Add to database and change adddbcontext

---
+++ JaysBook.Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations;
+
+namespace JaysBook.Models
+{
+    public class Category
+    {
+        [Key]
+        [Display(Name = "Category Name")]
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+    }
+}
